25b264fb5b98d01283c2ddc9df48763c548ce39e
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-01 22:23:30 +0100

    Dodao jos bootstrapa na park stranicu

--- StreetWorkoutWebApp/Controllers/ParkController.cs
+++ StreetWorkoutWebApp/Controllers/ParkController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StreetWorkoutWebApp.Data;
+using StreetWorkoutWebApp.Models;
 
 namespace StreetWorkoutWebApp.Controllers
 {
     public class ParkController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ParkController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Park> parks = _context.Parks.ToList();
+            return View(parks);
         }
     }
 }
