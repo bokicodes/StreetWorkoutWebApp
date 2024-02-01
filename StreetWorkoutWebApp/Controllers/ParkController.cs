6e81e8ae8c39ca6e06e6990f7247f80f3a332076
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-01 21:42:51 +0100

    Dodao controller i vew za Park

---
+++ StreetWorkoutWebApp/Controllers/ParkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StreetWorkoutWebApp.Controllers
+{
+    public class ParkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
