1ae45bbd23b1d05c2bbd5ec6276f4caef5f1bcf4
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-07 20:07:55 +0100

    Injectovao repository patern u program.cs fajl

--- StreetWorkoutWebApp/Program.cs
+++ StreetWorkoutWebApp/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using StreetWorkoutWebApp.Data;
+using StreetWorkoutWebApp.Interfaces;
+using StreetWorkoutWebApp.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IParkRepository, ParkRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
