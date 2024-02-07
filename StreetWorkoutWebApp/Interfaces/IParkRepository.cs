bda33ab363f1e67f311731848d7953fe9854ac38
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-07 19:39:48 +0100

    Napravio interfejs IParkRepository uz pomoc koga cu da implementiram repository pattern, dodao metode

---
+++ StreetWorkoutWebApp/Interfaces/IParkRepository.cs
@@ -0,0 +1,15 @@
+﻿using StreetWorkoutWebApp.Models;
+
+namespace StreetWorkoutWebApp.Interfaces
+{
+    public interface IParkRepository
+    {
+        Task<IEnumerable<Park>> GetAll();
+        Task<Park> GetByIdAsync(int id);
+        Task<IEnumerable<Park>> GetParksByCity(string city);
+        bool Add(Park park);
+        bool Update(Park park);
+        bool Delete(Park park);
+        bool Save();
+    }
+}
