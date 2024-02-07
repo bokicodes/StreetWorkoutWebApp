f25434a3368f9b930232072c0b49a16743b56eb8
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-07 20:04:18 +0100

    Implementirao metode interfejsa u klasi ParkRepository

---
+++ StreetWorkoutWebApp/Repository/ParkRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StreetWorkoutWebApp.Data;
+using StreetWorkoutWebApp.Interfaces;
+using StreetWorkoutWebApp.Models;
+
+namespace StreetWorkoutWebApp.Repository
+{
+    public class ParkRepository : IParkRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ParkRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Park park)
+        {
+            _context.Add(park);
+            return Save();
+        }
+
+        public bool Delete(Park park)
+        {
+            _context.Remove(park);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Park>> GetAll()
+        {
+            return await _context.Parks.ToListAsync();
+        }
+
+        public async Task<Park> GetByIdAsync(int id)
+        {
+            return await _context.Parks.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Park>> GetParksByCity(string city)
+        {
+            return await _context.Parks.Where(p => p.Address.City.Contains(city)).ToListAsync();
+        }
+
+        public bool Save()
+        {
+            int saved = _context.SaveChanges();
+            return saved > 0;
+        }
+
+        public bool Update(Park park)
+        {
+            _context.Update(park);
+            return Save();
+        }
+    }
+}
