a3717ad0ceefb7ed449134c97bc543913e9d3659
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-12 16:35:03 +0100

    Dodao logiku controlleru za registraciju

--- StreetWorkoutWebApp/Controllers/AccountController.cs
+++ StreetWorkoutWebApp/Controllers/AccountController.cs
@@ -69,5 +69,35 @@ namespace StreetWorkoutWebApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+                return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+
+            if(user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new AppUser
+            {
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+
+            var result = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if(result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+
+            return View("RegisterCompleted");
+        }
     }
 }
