57b66f3793a582f3454d736761905445c0b2a6d9
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-12 14:30:12 +0100

    Dodao logiku za post login u controleru

--- StreetWorkoutWebApp/Controllers/AccountController.cs
+++ StreetWorkoutWebApp/Controllers/AccountController.cs
@@ -25,9 +25,42 @@ namespace StreetWorkoutWebApp.Controllers
         }
 
         [HttpPost]
-        public async IActionResult Login(LoginVM loginVM)
+        public async Task<IActionResult> Login(LoginVM loginVM)
         {
+            if (!ModelState.IsValid)
+                return View(loginVM);
 
+            AppUser user = await _userManager.FindByEmailAsync(loginVM.Email);
+
+            if(user != null)
+            {
+                bool passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+
+                if(passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Park");
+                    }
+                    else
+                    {
+                        TempData["Error"] = "Sign in failed.";
+                        return View(loginVM);
+                    }
+                }
+                else
+                {
+                    TempData["Error"] = "Wrong password. Please, try again.";
+                    return View(loginVM);
+                }
+            }
+            else
+            {
+                TempData["Error"] = "Wrong credentials. Please, try again.";
+                return View(loginVM);
+            }
         }
 
     }
