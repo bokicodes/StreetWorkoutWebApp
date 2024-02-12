eb5da507489c16b9078d3aeb6432bda7309da9ec
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-12 14:02:27 +0100

    Podesio login controller, dodao view i viewmodel

---
+++ StreetWorkoutWebApp/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StreetWorkoutWebApp.Data;
+using StreetWorkoutWebApp.Models;
+
+namespace StreetWorkoutWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginVM loginVM)
+        {
+
+        }
+
+    }
+}
---
+++ StreetWorkoutWebApp/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StreetWorkoutWebApp.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        [Display(Name = "Email Address")]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
