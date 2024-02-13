fee43b7d50f0c00c7b11fae701d1db0b8545509f
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-13 14:49:16 +0100

    Dodao autentikaciju u program.cs

--- StreetWorkoutWebApp/ViewModels/RegisterVM.cs
+++ StreetWorkoutWebApp/ViewModels/RegisterVM.cs
@@ -9,8 +9,10 @@ namespace StreetWorkoutWebApp.ViewModels
         [Display(Name = "Email address")]
         public string EmailAddress { get; set; }
         [Required(ErrorMessage = "Password is required")]
+        [StringLength(20,MinimumLength = 6, ErrorMessage = "Password must have between 6 and 20 characters")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{6,}$", ErrorMessage = "" +
+            "Password must contain an uppercase character, lowercase character, a digit, and a non-alphanumeric character.")]
         [DataType(DataType.Password)]
-        [StringLength(20, MinimumLength = 5)]
         public string Password { get; set; }
         [Required(ErrorMessage = "Confirm password is required")]
         [Compare("Password",ErrorMessage = "Passwords do not match!")]
