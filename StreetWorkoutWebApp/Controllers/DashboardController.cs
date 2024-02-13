cefb2649a2d835e4828785fe6d3efd614a9467a1
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-13 15:32:14 +0100

    Napravio Dashboard controller

---
+++ StreetWorkoutWebApp/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StreetWorkoutWebApp.Interfaces;
+
+namespace StreetWorkoutWebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IDashboardRepository _dashboardRepository;
+
+        public DashboardController(IDashboardRepository dashboardRepository)
+        {
+            _dashboardRepository = dashboardRepository;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
