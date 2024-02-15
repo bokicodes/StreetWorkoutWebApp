3dd1a9f521dd112f05d27e9d246e3ee301787f15
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-15 20:14:24 +0100

    Dodao tri nova svojstva koja zelim da cuvam u bazi za korisnika

--- StreetWorkoutWebApp/Models/AppUser.cs
+++ StreetWorkoutWebApp/Models/AppUser.cs
@@ -6,6 +6,9 @@ namespace StreetWorkoutWebApp.Models
 {
     public class AppUser : IdentityUser
     {
+        public string? ProfileImageUrl { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
         [ForeignKey("Address")]
         public int? AddressId { get; set; }
         public Address? Address { get; set; }
