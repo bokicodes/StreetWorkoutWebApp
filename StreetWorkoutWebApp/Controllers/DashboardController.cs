631821809e4f9856022f950659e06745464f06d5
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-15 19:55:32 +0100

    Injectovao httpContext i photo servis u dashboard controller

--- StreetWorkoutWebApp/Controllers/DashboardController.cs
+++ StreetWorkoutWebApp/Controllers/DashboardController.cs
@@ -7,10 +7,14 @@ namespace StreetWorkoutWebApp.Controllers
     public class DashboardController : Controller
     {
         private readonly IDashboardRepository _dashboardRepository;
+        private readonly IHttpContextAccessor _httpContext;
+        private readonly IPhotoService _photoService;
 
-        public DashboardController(IDashboardRepository dashboardRepository)
+        public DashboardController(IDashboardRepository dashboardRepository, IHttpContextAccessor httpContext, IPhotoService photoService)
         {
             _dashboardRepository = dashboardRepository;
+            _httpContext = httpContext;
+            _photoService = photoService;
         }
 
 
@@ -24,6 +28,11 @@ namespace StreetWorkoutWebApp.Controllers
             };
 
             return View(dashboardVM);
+        }
+
+        public async Task<IActionResult> EditUserProfile()
+        {
+            return View();
         }
     }
 }
