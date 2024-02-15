80f48b34ce646bf89485b0d049325f6c21a43f6d
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-15 20:07:29 +0100

    Napravio view model za editovanje profila korisnika

---
+++ StreetWorkoutWebApp/ViewModels/EditUserProfileVM.cs
@@ -0,0 +1,11 @@
+﻿namespace StreetWorkoutWebApp.ViewModels
+{
+    public class EditUserProfileVM
+    {
+        public string Id { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
