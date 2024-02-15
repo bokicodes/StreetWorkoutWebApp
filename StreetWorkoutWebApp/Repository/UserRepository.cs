01e90e0a89666bfd74e1c2307375c37f3de4ada6
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-15 18:46:37 +0100

    Implementirao interfejs u user repository

---
+++ StreetWorkoutWebApp/Repository/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StreetWorkoutWebApp.Data;
+using StreetWorkoutWebApp.Interfaces;
+using StreetWorkoutWebApp.Models;
+
+namespace StreetWorkoutWebApp.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<AppUser>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserById(string id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0;
+        }
+
+        public bool Update(AppUser user)
+        {
+            _context.Update(user);
+            return Save();
+        }
+    }
+}
