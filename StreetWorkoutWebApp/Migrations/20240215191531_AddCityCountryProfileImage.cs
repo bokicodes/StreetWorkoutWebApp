6912b7c73dfd91dea5af0680b997a2ca35820c4d
bokycodes <114671954+bokycodes@users.noreply.github.com>
2024-02-15 20:16:02 +0100

    Dodao migraciju za nova svojstva korisnika

---
+++ StreetWorkoutWebApp/Migrations/20240215191531_AddCityCountryProfileImage.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreetWorkoutWebApp.Migrations
+{
+    public partial class AddCityCountryProfileImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
